8878a50c0ece4174503eec52f2e793adc8e609e7
exequiel <conan34681335@gmail.com>
2023-05-24 20:45:27 -0300

    maquetado primera parte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Componentes/home/home.component';
+import { NavBarComponent } from './ComponentesReutilizables/nav-bar/nav-bar.component';
+import { FormComponent } from './ComponentesReutilizables/form/form.component';
+import { InicioComponent } from './Componentes/inicio/inicio.component';
+import { MascotasPerdidasComponent } from './Componentes/mascotas-perdidas/mascotas-perdidas.component';
+import { MascotasEncontradasComponent } from './Componentes/mascotas-encontradas/mascotas-encontradas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavBarComponent,
+    FormComponent,
+    InicioComponent,
+    MascotasPerdidasComponent,
+    MascotasEncontradasComponent
   ],
   imports: [
     BrowserModule
